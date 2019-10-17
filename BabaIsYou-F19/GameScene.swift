97d777a36d70a03111e7927b0f40ef4d1692ae19
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-10-17 11:47:21 -0400

    R1: Implemented Baba - Can Perform actions like pushing an object and to move up, down, left and right.

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -10,9 +10,14 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var baba:SKSpriteNode!
+    let BABA_SPEED:CGFloat = 20
 
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
+        
+        self.baba = self.childNode(withName: "baba") as? SKSpriteNode
     }
    
     func didBegin(_ contact: SKPhysicsContact) {
@@ -24,6 +29,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // GET THE POSITION WHERE THE MOUSE WAS CLICKED
+        // ---------------------------------------------
+        let mouseTouch = touches.first
+        if (mouseTouch == nil) {
+                return
+            }
+        let location = mouseTouch!.location(in: self)
+
+        // WHAT NODE DID THE PLAYER TOUCH
+        // ----------------------------------------------
+        let nodeTouched = atPoint(location).name
+        //print("Player touched: \(nodeTouched)")
+               
+               
+        // GAME LOGIC: Move player based on touch
+        if (nodeTouched == "upButton") {
+            // move up
+            self.baba.position.y = self.baba.position.y + BABA_SPEED
+        }
+        else if (nodeTouched == "downButton") {
+            // move down
+            self.baba.position.y = self.baba.position.y - BABA_SPEED
+        }
+        else if (nodeTouched == "leftButton") {
+            // move left
+            self.baba.position.x = self.baba.position.x - BABA_SPEED
+        }
+        else if (nodeTouched == "rightButton") {
+            // move right
+            self.baba.position.x = self.baba.position.x + BABA_SPEED
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
