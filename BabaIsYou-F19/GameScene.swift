9bb0403b0b755cd674904d002c735f6d683ea214
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-10-17 22:19:37 -0400

    Assigned each wall different name. Added Condition for "WALLS IS STOP" rule. When "WALLS IS STOP" - Baba cannot go through Walls - GREEN

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -14,7 +14,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var baba:SKSpriteNode!
     let BABA_SPEED:CGFloat = 30
     
-    var wall:SKSpriteNode!
+    var wall1:SKSpriteNode!
+    var wall2:SKSpriteNode!
+    var wall3:SKSpriteNode!
+    var wall4:SKSpriteNode!
+    
     var winblock:SKSpriteNode!
     var stopblock:SKSpriteNode!
     var wallblock:SKSpriteNode!
@@ -29,11 +33,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //BABA - PLAYER
         self.baba = self.childNode(withName: "baba") as? SKSpriteNode
         self.baba.physicsBody?.categoryBitMask = 1
+        self.baba.physicsBody?.isDynamic = true
         
         //WALLS
-        self.wall = self.childNode(withName: "wall") as? SKSpriteNode
-        self.wall.physicsBody?.isResting = true
-        self.wall.physicsBody?.categoryBitMask = 2
+        self.wall1 = self.childNode(withName: "wall1") as? SKSpriteNode
+        self.wall2 = self.childNode(withName: "wall2") as? SKSpriteNode
+        self.wall3 = self.childNode(withName: "wall3") as? SKSpriteNode
+        self.wall4 = self.childNode(withName: "wall4") as? SKSpriteNode
+        
+        //self.wall.physicsBody?.isResting = true
+//        self.wall1.physicsBody?.categoryBitMask = 2
+//        self.wall2.physicsBody?.categoryBitMask = 2
+//        self.wall3.physicsBody?.categoryBitMask = 2
+//        self.wall4.physicsBody?.categoryBitMask = 2
         
         
         //ISBLOCK
@@ -43,22 +55,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //WIN BLOCK
         self.wallblock = self.childNode(withName: "wallblock") as? SKSpriteNode
         self.wallblock.physicsBody?.categoryBitMask = 8
+        self.wallblock.physicsBody?.isDynamic = true
         
         //FLAG BLOCK
         self.flagblock = self.childNode(withName: "flagblock") as? SKSpriteNode
         self.flagblock.physicsBody?.categoryBitMask = 16
+        self.flagblock.physicsBody?.isDynamic = true
         
         //STOP BLOCK
         self.stopblock = self.childNode(withName: "stopblock") as? SKSpriteNode
-        self.flagblock.physicsBody?.categoryBitMask = 32
+        self.stopblock.physicsBody?.categoryBitMask = 32
+        self.stopblock.physicsBody?.isDynamic = true
         
         //WIN BLOCK
         self.winblock = self.childNode(withName: "winblock") as? SKSpriteNode
-        self.flagblock.physicsBody?.categoryBitMask = 64
+        self.winblock.physicsBody?.categoryBitMask = 64
+        self.winblock.physicsBody?.isDynamic = true
         
         //FLAG
         self.flag = self.childNode(withName: "flag") as? SKSpriteNode
-        self.flagblock.physicsBody?.categoryBitMask = 128
+        self.flag.physicsBody?.categoryBitMask = 128
         
     }
    
@@ -81,24 +97,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             print("BABA WILL MOVE THROUGH WALLS")
         }
  */
-        
-         // Detect when WALL, IS and  STOP collide
-        if ((self.wallblock.frame.intersects(self.isblock.frame)) && (self.isblock.frame.intersects(self.stopblock.frame))==true)
-        {
-            print("WALLS WILL STOP THE BABA")
-            self.wall.physicsBody = SKPhysicsBody(rectangleOf: wall.size)
-            self.wall.physicsBody?.affectedByGravity = false
-            self.wall.physicsBody?.allowsRotation = false
-            self.wall.physicsBody?.isDynamic = false
-        }
-        
-    
-        
              
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+
+        // Detect when WALL, IS and  STOP collide
+        if ((self.wallblock.frame.intersects(self.isblock.frame)) && (self.isblock.frame.intersects(self.stopblock.frame))==true)
+        {
+            print("WALLS WILL STOP THE BABA")
+            self.wall1.physicsBody = SKPhysicsBody(rectangleOf: wall1.size)
+            self.wall2.physicsBody = SKPhysicsBody(rectangleOf: wall2.size)
+            self.wall3.physicsBody = SKPhysicsBody(rectangleOf: wall3.size)
+            self.wall4.physicsBody = SKPhysicsBody(rectangleOf: wall4.size)
+            
+            self.wall1.physicsBody?.affectedByGravity = false
+            self.wall2.physicsBody?.affectedByGravity = false
+            self.wall3.physicsBody?.affectedByGravity = false
+            self.wall4.physicsBody?.affectedByGravity = false
+
+            self.wall1.physicsBody?.allowsRotation = false
+            self.wall2.physicsBody?.allowsRotation = false
+            self.wall3.physicsBody?.allowsRotation = false
+            self.wall4.physicsBody?.allowsRotation = false
+
+            self.wall1.physicsBody?.isDynamic = false
+            self.wall2.physicsBody?.isDynamic = false
+            self.wall3.physicsBody?.isDynamic = false
+            self.wall4.physicsBody?.isDynamic = false
+        }
+        else{
+            print("BABA WILL GOTHROUGH THE WALLS")
+           
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
